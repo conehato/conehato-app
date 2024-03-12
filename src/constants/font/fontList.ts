ca3970c66d87f69539488ac2891917d8b140eb45
Junseo Oh <40460655+outsung@users.noreply.github.com>
2024-03-12 21:31:29 +0900

    Fix: changed `.ttf` to `.woff2`

--- src/constants/font/fontList.ts
+++ src/constants/font/fontList.ts
@@ -1,7 +1,7 @@
-// @index('../../assets/fonts', f => `export { default as ${f.path.replace('../../assets/fonts/', '').replace('-', '_')} } from '${f.path}.ttf';`)
-export { default as SpoqaHanSansNeo_bold } from '../../assets/fonts/SpoqaHanSansNeo-bold.ttf';
-export { default as SpoqaHanSansNeo_light } from '../../assets/fonts/SpoqaHanSansNeo-light.ttf';
-export { default as SpoqaHanSansNeo_medium } from '../../assets/fonts/SpoqaHanSansNeo-medium.ttf';
-export { default as SpoqaHanSansNeo_semiBold } from '../../assets/fonts/SpoqaHanSansNeo-semiBold.ttf';
-export { default as SpoqaHanSansNeo_thin } from '../../assets/fonts/SpoqaHanSansNeo-thin.ttf';
+// @index('../../assets/fonts', f => `export { default as ${f.path.replace('../../assets/fonts/', '').replace('-', '_')} } from '${f.path}.woff2';`)
+export { default as SpoqaHanSansNeo_bold } from '../../assets/fonts/SpoqaHanSansNeo-bold.woff2';
+export { default as SpoqaHanSansNeo_light } from '../../assets/fonts/SpoqaHanSansNeo-light.woff2';
+export { default as SpoqaHanSansNeo_medium } from '../../assets/fonts/SpoqaHanSansNeo-medium.woff2';
+export { default as SpoqaHanSansNeo_semiBold } from '../../assets/fonts/SpoqaHanSansNeo-semiBold.woff2';
+export { default as SpoqaHanSansNeo_thin } from '../../assets/fonts/SpoqaHanSansNeo-thin.woff2';
 // @endindex
