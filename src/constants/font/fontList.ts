c76d57b449464d964d568f9be1f6689558eea704
Junseo Oh <40460655+outsung@users.noreply.github.com>
2024-03-12 21:29:15 +0900

    Feat: change new font

--- src/constants/font/fontList.ts
+++ src/constants/font/fontList.ts
@@ -1,7 +1,7 @@
 // @index('../../assets/fonts', f => `export { default as ${f.path.replace('../../assets/fonts/', '').replace('-', '_')} } from '${f.path}.ttf';`)
-export { default as NotoSansJP_bold } from '../../assets/fonts/NotoSansJP-bold.ttf';
-export { default as NotoSansJP_light } from '../../assets/fonts/NotoSansJP-light.ttf';
-export { default as NotoSansJP_medium } from '../../assets/fonts/NotoSansJP-medium.ttf';
-export { default as NotoSansJP_semiBold } from '../../assets/fonts/NotoSansJP-semiBold.ttf';
-export { default as NotoSansJP_thin } from '../../assets/fonts/NotoSansJP-thin.ttf';
+export { default as SpoqaHanSansNeo_bold } from '../../assets/fonts/SpoqaHanSansNeo-bold.ttf';
+export { default as SpoqaHanSansNeo_light } from '../../assets/fonts/SpoqaHanSansNeo-light.ttf';
+export { default as SpoqaHanSansNeo_medium } from '../../assets/fonts/SpoqaHanSansNeo-medium.ttf';
+export { default as SpoqaHanSansNeo_semiBold } from '../../assets/fonts/SpoqaHanSansNeo-semiBold.ttf';
+export { default as SpoqaHanSansNeo_thin } from '../../assets/fonts/SpoqaHanSansNeo-thin.ttf';
 // @endindex
